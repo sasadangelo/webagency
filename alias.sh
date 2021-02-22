13591022ef2abc2d2b8a9d689887be8f652f0e59
sasadangelo <sasadangelo@gmail.com>
2021-02-22 16:32:23 +0100

    Install theme and plugins automatically

---
+++ alias.sh
@@ -0,0 +1,1 @@
+alias wp="docker run -it --rm -e HOME=/tmp --dns=8.8.8.8 --network webagency-net -v wordpress_volume:/var/www/html -u xfs wordpress:cli"
--- configure_wp.sh
+++ configure_wp.sh
@@ -5,3 +5,12 @@ source $SCRIPT_DIR/.env
 wp core install --url=$VIRTUAL_HOST --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_USER --admin_password=$WORDPRESS_PASSWD --admin_email=$WORDPRESS_USER_EMAIL; 
 wp plugin delete hello akismet;
 wp theme delete twentynineteen twentytwenty
+for plugin in $WORDPRESS_PLUGINS; do
+    echo "wp plugin install $plugin --activate"
+    wp plugin install $plugin --activate
+done
+if [ ! -z "$WORDPRESS_THEME" ]
+then
+    wp theme install $WORDPRESS_THEME --activate
+    wp theme delete twentytwentyone
+fi
